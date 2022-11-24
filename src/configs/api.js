8adfaf94d1bffc84b757dab3207ffdab2e65f43a
Alan Guedes <alanguedes00@live.com>
2022-11-24 16:24:27 -0300

    update

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,6 +1,6 @@
 
-export const URI_BASE_API   = `http://127.0.0.1:9001` // DEV
-//export const URI_BASE_API   = `https://admin.geradordesistemas.com.br` // PROD
+//export const URI_BASE_API   = `http://127.0.0.1:9001` // DEV
+export const URI_BASE_API   = `https://admin.geradordesistemas.com.br` // PROD
 export const API_VERSION    = 'api'
 export const TOKEN_NAME     = 'TOKEN_JWT'
 
